63a78fb0d14728489c0f0b284143fcf61030074e
milindsoorya <m.soorya@shopper.com>
2021-06-19 21:27:06 +0530

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import "styles/global.scss";
+import * as ga from "lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
