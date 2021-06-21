ba7b81a80a54e1920a2fcd1af13b5534128bd349
milindsoorya <m.soorya@shopper.com>
2021-06-21 12:27:42 +0530

    ackee to vercel app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // import { useEffect } from "react";
-// import { useRouter } from "next/router";
+import { useRouter } from "next/router";
 
 import "styles/global.scss";
 // import * as ga from "lib/ga";
@@ -18,6 +18,28 @@ function MyApp({ Component, pageProps }) {
   //     router.events.off("routeChangeComplete", handleRouteChange);
   //   };
   // }, [router.events]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      useAckee(
+        url,
+        {
+          server: "https://www.picturetalkies.in/",
+          domainId: "f82d35a6-5e5f-405d-85c2-3e9a58e33ad2",
+        },
+        {
+          detailed: false,
+          ignoreLocalhost: true,
+          ignoreOwnVisits: false,
+        }
+      );
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
